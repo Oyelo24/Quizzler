5c9a3ec571352334077cdbb7f71e5c07df967881
Olayiwola Oyelowo <oyewolasolomon78@gmail.com>
2025-08-12 12:36:29 +0100

    added question and answer list

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreTrack = [
-    Icon(Icons.check, color: Colors.green),
-    Icon(Icons.close, color: Colors.red),
+  List<Icon> scoreTrack = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
+ 
+  List<bool> answers = [false, true, true];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -46,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -63,9 +70,16 @@ class _QuizPageState extends State<QuizPage> {
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
                 //The user picked true.
+                if (correctAnswer == true){
+                  print('User is correct');
+                }else{
+                  print('User is wrong');
+                }
                 setState(() {
-                  scoreTrack.add(Icon(Icons.check, color: Colors.green));
+                  questionNumber++;
+                  //scoreTrack.add(Icon(Icons.check, color: Colors.green));
                 });
               },
             ),
@@ -81,15 +95,22 @@ class _QuizPageState extends State<QuizPage> {
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
                 //The user picked false.
+                if(correctAnswer == false){
+                  print('user is correct');
+                } else{
+                  print('user is wrong');
+                }
                 setState(() {
-                  scoreTrack.add(Icon(Icons.close, color: Colors.red));
+                  questionNumber ++;
+                  //scoreTrack.add(Icon(Icons.close, color: Colors.red));
                 });
               },
             ),
           ),
         ),
-        Row(children: scoreTrack),
+        //Row(children: scoreTrack),
       ],
     );
   }
