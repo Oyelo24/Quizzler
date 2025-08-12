bd86b7c3714fff28fd923f585801cb81d54a321b
Olayiwola Oyelowo <oyewolasolomon78@gmail.com>
2025-08-12 10:23:05 +0100

    added score track

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreTrack = [
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.close, color: Colors.red),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -60,6 +64,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreTrack.add(Icon(Icons.check, color: Colors.green));
+                });
               },
             ),
           ),
@@ -75,11 +82,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreTrack.add(Icon(Icons.close, color: Colors.red));
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scoreTrack),
       ],
     );
   }
